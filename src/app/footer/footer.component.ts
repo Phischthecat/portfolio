acdc70b9854a22719a85e06992449e5b87e51a53
Phischthecat <p.schmuck.er@web.de>
2025-02-05 21:19:29 +0100

    update year in footer automatically

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,18 +2,23 @@ import { Component, Input } from '@angular/core';
 import { IsOpenService } from '../is-open.service';
 
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss'],
-    standalone: false
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: false,
 })
 export class FooterComponent {
-
-  constructor(public menu : IsOpenService){}
-
-  scrollToStart() {   
-    window.scrollTo(0, 0);    
+  year = 2025;
+  constructor(public menu: IsOpenService) {
+    this.year = this.actualYear();
   }
 
- 
+  scrollToStart() {
+    window.scrollTo(0, 0);
+  }
+
+  actualYear() {
+    const year = new Date();
+    return year.getFullYear();
+  }
 }
