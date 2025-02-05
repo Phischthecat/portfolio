0e9faeb8994cd3180dc252891a5991bc8980297f
Phischthecat <p.schmuck.er@web.de>
2025-02-05 22:28:36 +0100

    tiny changes

--- src/app/socials/socials.component.ts
+++ src/app/socials/socials.component.ts
@@ -1,27 +1,30 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-socials',
-    templateUrl: './socials.component.html',
-    styleUrls: ['./socials.component.scss'],
-    standalone: false
+  selector: 'app-socials',
+  templateUrl: './socials.component.html',
+  styleUrls: ['./socials.component.scss'],
+  standalone: false,
 })
 export class SocialsComponent {
-  socials = [{
-    link: 'https://github.com/Phischthecat',
-    target: '_blank',
-    logo: 'github.svg',
-    name: 'github'
-   },{
-    link: '#contact',
-    target: '',
-    logo: 'email.svg',
-    name: 'email'
-   },{
-    link: 'https://www.linkedin.com/in/phil-schmucker-421b17181/',
-    target: '_blank',
-    logo: 'linkedin.svg',
-    name: 'linkedin'
-   }];
-
+  socials = [
+    {
+      link: 'https://github.com/Phischthecat',
+      target: '_blank',
+      logo: 'github.svg',
+      name: 'github',
+    },
+    {
+      link: '#contact',
+      target: '',
+      logo: 'email.svg',
+      name: 'email',
+    },
+    {
+      link: 'https://www.linkedin.com/in/phil-schmucker/',
+      target: '_blank',
+      logo: 'linkedin.svg',
+      name: 'linkedin',
+    },
+  ];
 }
