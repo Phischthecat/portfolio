dd12586698942dbf33b5ec4f33a0f702656d8264
Phischthecat <p.schmuck.er@web.de>
2025-02-06 22:20:30 +0100

    update skills, compress img, new i18n, new about me text

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-skills',
-    templateUrl: './skills.component.html',
-    styleUrls: ['./skills.component.scss'],
-    standalone: false
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
+  standalone: false,
 })
 export class SkillsComponent {
-  skills = ['Angular','Typescript','Javascript','HTML', 'CSS', 'Firebase','Git','Scrum','Rest-API', 'Material Design'];
-  skills_img = ['angular.svg','typescript.svg','javascript.svg','html.svg','css.svg','firebase.svg','git.svg','scrum.svg','rest-api.svg','material-design.svg'];
-
+  skills = [
+    'Angular',
+    'Typescript',
+    'Javascript',
+    'HTML',
+    'CSS',
+    'Firebase',
+    'Git',
+    'Scrum',
+    'Rest-API',
+    'Figma',
+    'Docker',
+  ];
+  skills_img = [
+    'angular.svg',
+    'typescript.svg',
+    'javascript.svg',
+    'html.svg',
+    'css.svg',
+    'firebase.svg',
+    'git.svg',
+    'scrum.svg',
+    'rest-api.svg',
+    'figma.svg',
+    'docker.png',
+  ];
 }
