c6afe6b30256357b15aafed1478e080d479940b5
Phischthecat <p.schmuck.er@web.de>
2026-01-13 22:52:14 +0100

    Update about-me section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,22 +1,44 @@
-import { Component, ViewChild, OnInit, ElementRef} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-about-me',
-    templateUrl: './about-me.component.html',
-    styleUrls: ['./about-me.component.scss'],
-    standalone: false
+  selector: 'app-about-me',
+  templateUrl: './about-me.component.html',
+  styleUrls: ['./about-me.component.scss'],
+  standalone: false,
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent {
+  isLoadingVideo = true;
+  private rewindInterval: any;
 
-  constructor() {   
-    
-  }
-  ngOnInit(){
-    
-  }
+  constructor() {}
 
   moveTo(section) {
     document.location = '#' + section;
   }
 
+  playVideo(video: HTMLVideoElement) {
+    this.stopRewind();
+    video.play().catch((err) => console.warn('Playback prevented:', err));
+  }
+
+  startRewind(video: HTMLVideoElement) {
+    video.pause();
+    this.stopRewind();
+
+    this.rewindInterval = setInterval(() => {
+      if (video.currentTime > 0) {
+        video.currentTime -= 0.1;
+      } else {
+        video.currentTime = 0;
+        this.stopRewind();
+      }
+    }, 100);
+  }
+
+  private stopRewind() {
+    if (this.rewindInterval) {
+      clearInterval(this.rewindInterval);
+      this.rewindInterval = null;
+    }
+  }
 }
